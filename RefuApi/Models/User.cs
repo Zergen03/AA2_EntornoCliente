c726f7944f14fd7b0ddfb1d93274bc55ae0b23d5
Zergen03 <alexizquierdo2003@gmail.com>
2025-04-16 13:23:38 +0200

    Initial commit

---
+++ RefuApi/Models/User.cs
@@ -0,0 +1,35 @@
+﻿namespace RefuApi.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]+    public string Password { get; set; }
+
+    public bool IsVeteran { get; set; }
+
+    public User() { }
+
+    public User(string name, string email, string password, bool isVeteran)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        IsVeteran = isVeteran;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Name: {Name}, Email: {Email}, IsVeteran: {IsVeteran}";
+    }
+}
